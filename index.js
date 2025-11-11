74fab901f890e2d911f7ae4aadb4abc846d32f88
Arghya044 <arghya.rkbk19@gmail.com>
2025-11-12 01:59:54 +0600

    middleware and URi conection

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.MONGODB_URI;
+
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -17,13 +23,13 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    
     await client.connect();
-    // Send a ping to confirm a successful connection
+    
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
+   
     await client.close();
   }
 }
